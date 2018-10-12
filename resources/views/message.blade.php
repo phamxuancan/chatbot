c07da0e18f3507c18599a39eeefffea465566786
canpx <canpx@ominext.com>
2018-10-12 10:14:02 +0700

    update facebook bt

---
+++ resources/views/message.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" type="text/css" href="css/message.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header text-align">
+            <div class="vertical-align bold">CHAT WITH MY PAGE</div>
+        </div>
+        <div class="content text-align">
+            <button class="btn-facebook">
+                <div><img class="facebook_icon" src="images/f_icon.png" /></div>
+                <div class="facebook_text bold" > Continue with Facebook</div>  </button>
+        </div>
+     </div>
+    
+</body>
+</html>
